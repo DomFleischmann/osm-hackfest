713d9c5e1927a1f19b8304c93acd4f682cb21902
Adam Israel <adam.israel@canonical.com>
2019-04-11 09:59:36 -0400

    Cleanup

---
+++ descriptor-packages/vnfd/simple_vnf/charms/simple/reactive/simple.py
@@ -0,0 +1,34 @@
+from charmhelpers.core.hookenv import (
+    action_get,
+    action_fail,
+    action_set,
+    status_set,
+)
+from charms.reactive import (
+    remove_state as remove_flag,
+    set_state as set_flag,
+    when,
+    when_not,
+)
+import charms.sshproxy
+
+
+@when_not('simple.installed')
+def install_simple_proxy_charm():
+    set_flag('simple.installed')
+    status_set('active', 'Ready!')
+
+
+@when('actions.touch')
+def touch():
+    err = ''
+    try:
+        filename = action_get('filename')
+        cmd = ['touch {}'.format(filename)]
+        result, err = charms.sshproxy._run(cmd)
+    except:
+        action_fail('command failed:' + err)
+    else:
+        action_set({'outout': result})
+    finally:
+        remove_flag('actions.touch')
